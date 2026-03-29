2ac26d155d368cff8e48fbfaf9ae1e04da5cf99e
barisayhan <baris.ayhan@lydians.com>
2026-03-29 19:39:09 +0300

    Spring Boot Actuator (monitoring için veri toplama)

--- src/main/java/com/security/config/SecurityConfig.java
+++ src/main/java/com/security/config/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/public/**").permitAll()
                         .requestMatchers("/api/v1/messages/**").permitAll()
                         .requestMatchers("/api/v1/redis/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         //    .requestMatchers("/api/v1/admin/**").hasRole("ADMIN") // Sadece Admin
                         .anyRequest().authenticated() // Geri kalan her yer için giriş şart
                 )
